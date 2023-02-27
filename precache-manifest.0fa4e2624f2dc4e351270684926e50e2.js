922e573f1f20f45b5bc58e233cef2503b38e9a99
Avinash Kumar Verma <avinashverma078@gmail.com>
2023-02-27 18:37:16 +0530

    Updates

---
+++ precache-manifest.0fa4e2624f2dc4e351270684926e50e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5b9a9355bb189625161",
+    "url": "/my-project/static/css/main.2120ab68.chunk.css"
+  },
+  {
+    "revision": "c5b9a9355bb189625161",
+    "url": "/my-project/static/js/main.c5b9a935.chunk.js"
+  },
+  {
+    "revision": "2b4e57923141e0aa228a",
+    "url": "/my-project/static/js/1.2b4e5792.chunk.js"
+  },
+  {
+    "revision": "2b4c9b704cd7c2c3a9be",
+    "url": "/my-project/static/js/2.2b4c9b70.chunk.js"
+  },
+  {
+    "revision": "8c8698ad2f3292ac46f4",
+    "url": "/my-project/static/js/runtime~main.8c8698ad.js"
+  },
+  {
+    "revision": "eb449b73b3142d65b1d953ca09f923c4",
+    "url": "/my-project/static/media/dxf91zhqd2z6b0bwg85ktm5s4.eb449b73.svg"
+  },
+  {
+    "revision": "205ad4baa938eba6585d328d39c9bb2f",
+    "url": "/my-project/index.html"
+  }
+];
